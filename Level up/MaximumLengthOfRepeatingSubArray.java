f3e5f62bce7058aa6f9ba1f8c93d7b234ea6cc3a
Raghav Bajaj <73180455+Raghav-Bajaj@users.noreply.github.com>
2021-07-12 00:28:43 +0530

    Maximum Length Of Repeated Subarray
    1. You are given two arrays of integers arr1 and arr2.
    2. You have to find the maximum length of subarray that appears in both the given arrays.

---
+++ Level up/MaximumLengthOfRepeatingSubArray.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static int solution(int[] arr1, int[] arr2) {
+   int[][] dp = new int[arr1.length+1][arr2.length+1];
+   int ans = 0 ; 
+   for(int i  = 1 ; i < dp.length ; i++)
+   {
+       for(int j = 1 ; j < dp[0].length ; j++)
+       {
+           if(arr1[i-1] == arr2[j-1])
+           {
+               dp[i][j] = 1 + dp[i-1][j-1];
+               ans = Math.max(dp[i][j] , ans);
+           }
+       }
+   }
+    return ans ;
+  }
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr1 = new int[n];
+    for (int i = 0 ; i < n; i++) {
+      arr1[i] = scn.nextInt();
+    }
+
+    int m = scn.nextInt();
+    int[] arr2 = new int[m];
+    for (int i = 0 ; i < m; i++) {
+      arr2[i] = scn.nextInt();
+    }
+    System.out.println(solution(arr1, arr2));
+  }
+
+}
